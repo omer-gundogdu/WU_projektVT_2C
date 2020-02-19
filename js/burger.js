8489f690cb9d8e4e4b75fabc66135ae55099bdd7
omer-gundogdu <omer-gundogdu@elev.ga.ntig.se>
2020-02-19 15:00:42 +0100

    Lektion v8

---
+++ js/burger.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector(".burger")
+
+
+function toggleMenu(){
+    document.querySelector(".navLinks"),classlist.toggle("navActive");
+
+    burger,classlist.toggle("toggle");
+
+}
+burger.addEventListener("click", toggleMenu)
