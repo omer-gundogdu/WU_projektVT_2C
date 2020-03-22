cf998adb45160b76645634df322782d21d96f117
omer-gundogdu <omer-gundogdu@users.noreply.github.com>
2020-03-22 18:26:56 +0100

    Fixade till css,js och index

--- js/script.js
+++ js/script.js
@@ -1,11 +1,53 @@
-const menuIcon = document.querySelector (".hamburger-menu");
+// Waits until the html is completely loaded to run the functions it has inside.
+window.addEventListener('DOMContentLoaded', () => {
+    scrollHandler();
+});
 
+const bd = document.body; // Body Tag Element
 const header = document.querySelector(".header");
-
+const menuIcon = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector(".navbar");
+const avoider = document.querySelector(".avoider");
+
+// Scroll Handler
+const startHidingPosition = 400; // <= you can change this
+let lastScrollTop = 0;
+
+function scrollHandler() {
+    window.addEventListener('scroll', (e) => {
+        let wy = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (wy > lastScrollTop && wy >= startHidingPosition) {
+            header.style.transform = "translateY(-100%)"
+            menuIcon.style.transform = "translateY(-400%)"
+        } else {
+            header.style.transform = "translateY(0)"
+            menuIcon.style.transform = "translateY(0)"
+        }
+
+        lastScrollTop = wy <= 0 ? 0 : wy;
+    });
+}
 
 
-menuIcon.addEventListener ("click", () => {
-    header.classList.toggle("change");
+// Hamburger Toggle
+
+menuIcon.addEventListener("click", () => {
+    toggleNav();
+});
+
+avoider.addEventListener('click', ()=> {
+    toggleNav();
+});
+
+function toggleNav() {
+    menuIcon.classList.toggle("hamburgerChange");
     navbar.classList.toggle("change");
- });
+    bd.classList.toggle("overflowChange");
+
+    if (avoider.style.display === "none") {
+        avoider.style.display = "block";
+    } else {
+        avoider.style.display = "none";
+    }
+}
