1936638caf5ebdf7e77b87005801ed104a9b7d5f
omer-gundogdu <omer-gundogdu@users.noreply.github.com>
2020-03-22 12:51:56 +0100

    Hamburger

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const menuIcon = document.querySelector (".hamburger-menu");
 
+const header = document.querySelector(".header");
+
 const navbar = document.querySelector(".navbar");
 
 
 menuIcon.addEventListener ("click", () => {
+    header.classList.toggle("change");
     navbar.classList.toggle("change");
-})
+ });
